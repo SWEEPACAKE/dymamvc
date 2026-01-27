ca65e2e9c3b3da463e398467657a30bd9320c756
SWEEPACAKE <nicolas.savoca.info@gmail.com>
2026-01-27 11:46:53 +0100

    Anonymisation de la config

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 // La méthode "define" permet de définir une constante. En premier paramètre, le nom de la constante, puis ensuite sa valeur
-define('DB_HOST', '192.168.56.56');
-define('DB', 'mvc');
-define('DB_USER', 'homestead');
-define('DB_PASSWORD', 'secret');
+define('DB_HOST', '');
+define('DB', '');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
